f30d2178222104692dd5818fe7e9f3b4743e6f60
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-11-21 07:50:48 +0200

    commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -20,20 +20,44 @@
 
 import TableBody from "./TableBody";
 import TableHead from "./TableHead";
-import { useSortableTable } from "../useSortableTable";
+import {useSortableTable} from "../useSortableTable";
+import {DragDropContext} from "react-beautiful-dnd";
+import {useState} from "react";
 
-const Table = ({ caption, data, columns }) => {
-  const [tableData, handleSorting] = useSortableTable(data, columns);
+const Table = ({caption, data, columns}) => {
+    const [tableData, handleSorting] = useSortableTable(data, columns);
+    const [finalTableData, setFinalTableData]  = useState(tableData);
 
-  return (
-    <>
-      <table className="table">
-        <caption>{caption}</caption>
-        <TableHead {...{ columns, handleSorting }} />
-        <TableBody {...{ columns, tableData }} />
-      </table>
-    </>
-  );
+// function handleDragEnd (e) {
+//     if (!e.destination) return;
+//     let tempData = Array.from(finalTableData);
+//     let [source_data] = tempData.splice(e.source.index, 1);
+//     tempData.splice(e.destination.index, 0, source_data);
+//     setFinalTableData(tempData);
+//   }
+
+function handleDragEnd (e) {
+    if (!e.destination) return;
+    let tempData = Array.from(finalTableData);
+    let [source_data] = tempData.splice(e.source.index, 1);
+    tempData.splice(e.destination.index, 0, source_data);
+    setFinalTableData(tempData);
+  }
+
+    return (
+        <div>
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <table className="table">
+                    <caption>{caption}</caption>
+                    <DragDropContext>
+                        <TableHead {...{columns, handleSorting}} />
+
+                        <TableBody {...{columns, tableData:finalTableData}} />
+                    </DragDropContext>
+                </table>
+            </DragDropContext>
+            </div>
+    );
 };
 
 export default Table;
