a22f3c4fd88dfa58ac48757b68b1559e4a2a119c
Halia <91005548+Haliaa@users.noreply.github.com>
2024-06-07 18:25:25 +0300

    make it stick on click release

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,52 +1,26 @@
 import TableBody from "./TableBody";
-import {TableHead} from "./TableHead";
-import {useSortableTable} from "../useSortableTable";
-import {DragDropContext} from "react-beautiful-dnd";
-import {
-    useState,
-    useRef,
-    // useEffect
-} from "react";
+import { TableHead } from "./TableHead";
+import { useSortableTable } from "../useSortableTable";
 
-const Table = ({caption, data, columns}) => {
-    const [tableData, handleSorting] = useSortableTable(data, columns);
+const Table = ({ caption, data, columns }) => {
+  const [tableData, handleSorting] = useSortableTable(data, columns);
 
-    const [users, setUsers] = useState([...data]);
-
-    // const prevCountRef = useRef();
-    // const [boolean, setBoolean] = useState();
-    // useEffect(() => {
-    //     prevCountRef.current = boolean;
-    //
-    //     if(prevCountRef.current==='true'){
-    //
-    //     }
-    // }, [boolean]);
-
-    const handleDragEnd = (result) => {
-        const tempUser = users;
-            const [selectedRow] = tempUser.splice(result.source.index, 1);
-            tempUser.splice(result.destination.index, 0, selectedRow)
-            setUsers(tempUser)
-    };
-
-    return (
-        <div>
-            <div>
-                <table className="table">
-                    <caption>{caption}</caption>
-                    <DragDropContext onDragEnd={handleDragEnd}>
-                        <TableHead {...{
-                            columns, handleSorting,
-                            // setBoolean
-                        }} />
-
-                        <TableBody {...{columns, tableData}} />
-                    </DragDropContext>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <table className="table">
+          <caption>{caption}</caption>
+          <TableHead
+            {...{
+              columns,
+              handleSorting,
+            }}
+          />
+          <TableBody {...{ columns, tableData }} />
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,42 +1,50 @@
-import {Draggable, Droppable} from "react-beautiful-dnd";
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-const TableBody = ({columns, tableData}) => {
-    return (
-        <Droppable droppableId="tbody">
-            {(provided) => {
-                return (
-                    <tbody ref={provided.innerRef} {...provided.droppableProps}>
-                    {tableData.map((data, index) => {
-                        return (
-                            <Draggable
-                                draggableId={data.full_name}
-                                index={index}
-                                key={data.full_name}>
-                                {(provided) => {
-                                    return (
-                                        <tr
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            key={data.id}>
-                                            {columns.map(({accessor}) => {
-                                                const index = tableData.indexOf(data)+1;
-                                                const tData = data[accessor] ? data[accessor] : index;
-                                                return <td {...provided.dragHandleProps} key={accessor}>{tData}</td>;
-                                            })}
-                                        </tr>
-                                    )
-                                }}
-                            </Draggable>
-                        );
-                    })}
-                    {provided.placeholder}
-                    </tbody>
-                )
+const TableBody = ({ columns, tableData: initialTableData }) => {
+  const [tableData, setTableData] = useState(initialTableData);
 
-            }
-            }
-        </Droppable>
-    );
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = [...tableData];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setTableData(items);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="tbody">
+        {(provided) => (
+          <tbody ref={provided.innerRef} {...provided.droppableProps}>
+            {tableData.map((data, index) => (
+              <Draggable
+                draggableId={data.full_name}
+                index={index}
+                key={data.full_name}
+              >
+                {(provided) => (
+                  <tr
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    key={data.id}
+                  >
+                    {columns.map(({ accessor }) => (
+                      <td key={accessor}>{data[accessor]}</td>
+                    ))}
+                  </tr>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </tbody>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 };
 
 export default TableBody;
